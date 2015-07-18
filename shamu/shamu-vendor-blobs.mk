cjkacz/proprietary_vendor_moto

Sprint Prl update fix
Compiled and Tested by @BeansTown106

shamu/shamu-vendor-blobs.mk
@@ -15,6 +15,8 @@
 # This file is generated by device/moto/shamu/setup-makefiles.sh
 
 PRODUCT_COPY_FILES += \
+    vendor/moto/shamu/proprietary/priv-app/DMService/lib/arm/libdmengine.so:system/priv-app/DMService/lib/arm/libdmengine.so \
+    vendor/moto/shamu/proprietary/priv-app/DMService/lib/arm/libdmjavaplugin.so:system/priv-app/DMService/lib/arm/libdmjavaplugin.so \
     vendor/moto/shamu/proprietary/bin/adspd:system/bin/adspd \
     vendor/moto/shamu/proprietary/lib/libadspd-jni.so:system/lib/libadspd-jni.so \
     vendor/moto/shamu/proprietary/lib/libadspd.so:system/lib/libadspd.so \

Altaf-Mahdi/android_vendor_motorola

Conflicts:
	shamu/shamu-vendor-blobs.mk
Change-Id: I6e6e983f38c7398cb28c85b41f8e93323bc927a1

shamu/shamu-vendor-blobs.mk
@@ -15,6 +15,8 @@
 # This file is generated by device/moto/shamu/setup-makefiles.sh
 
 PRODUCT_COPY_FILES += \
+    vendor/motorola/shamu/proprietary/priv-app/DMService/lib/arm/libdmengine.so:system/priv-app/DMService/lib/arm/libdmengine.so \
+    vendor/motorola/shamu/proprietary/priv-app/DMService/lib/arm/libdmjavaplugin.so:system/priv-app/DMService/lib/arm/libdmjavaplugin.so \
     vendor/motorola/shamu/proprietary/bin/adspd:system/bin/adspd \
     vendor/motorola/shamu/proprietary/lib/libadspd-jni.so:system/lib/libadspd-jni.so \
     vendor/motorola/shamu/proprietary/lib/libadspd.so:system/lib/libadspd.so \